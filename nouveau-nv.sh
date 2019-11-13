68befec815598f4a9708b02765b53797fe82a21d
bigballon <fm.bigballon@gmail.com>
2019-11-13 19:47:08 +0800

    [update] change sudo right

--- nouveau-nv.sh
+++ nouveau-nv.sh
@@ -1,12 +1,13 @@
+sudo -s
 cd /etc/modprobe.d/
 touch disable-nouveau.conf
 echo "blacklist nouveau" >> disable-nouveau.conf
 echo "options nouveau modeset=0" >> disable-nouveau.conf
-sudo apt-get purge nvidia-*
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo apt-get update
+apt-get purge nvidia-*
+add-apt-repository ppa:graphics-drivers/ppa
+apt-get update
 
-sudo update-initramfs -u
-sudo reboot
+update-initramfs -u
+reboot
 
 
